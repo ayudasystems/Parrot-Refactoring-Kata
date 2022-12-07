55d8d3b055e95f4fdf02157e88798c57aaeae552
Alexandre Leblanc <alexandre.leblanc@broadsign.com>
2022-12-07 11:37:01 -0500

    CORE-XXXX: remove manual delete

--- cpp/gtest/parrot_gtest.cpp
+++ cpp/gtest/parrot_gtest.cpp
@@ -2,47 +2,45 @@
 
 using namespace std;
 
+#include <memory>
 #include "Parrot.h"
 
-
 TEST(ParrotTest, SpeedOfEuropeanParrot) {
-    auto *parrot = new Parrot(EUROPEAN, 0, 0, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(EUROPEAN, 0, 0, false));
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
-    delete parrot;
+}
+
+TEST(ParrotTest, SpeedOfEuropeanParrot_With_One_Coconut) {
+    std::unique_ptr<Parrot> parrot(new Parrot(EUROPEAN, 1, 0, false));
+    EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_One_Coconut) {
-    auto *parrot = new Parrot(AFRICAN, 1, 0, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(AFRICAN, 1, 0, false));
     EXPECT_NEAR(3.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_Two_Coconuts) {
-    auto *parrot = new Parrot(AFRICAN, 2, 0, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(AFRICAN, 2, 0, false));
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_No_Coconuts) {
-    auto *parrot = new Parrot(AFRICAN, 0, 0, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(AFRICAN, 0, 0, false));
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_nailed) {
-    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, true);
+    std::unique_ptr<Parrot> parrot(new Parrot(NORWEGIAN_BLUE, 0, 1.5, true));
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed) {
-    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(NORWEGIAN_BLUE, 0, 1.5, false));
     EXPECT_NEAR(18.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed_high_voltage) {
-    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 4, false);
+    std::unique_ptr<Parrot> parrot(new Parrot(NORWEGIAN_BLUE, 0, 4, false));
     EXPECT_NEAR(24.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
